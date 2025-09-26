82bd86a3ffed5fd4cd693b9bbab2bf84b3c5633c
Mizakson <mizakson.dev@gmail.com>
2025-09-26 13:16:04 -0400

    Properly render message from test route
    Retrieved from deployed back-end api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  useEffect(() => {
+    const dataFetchTest = async () => {
+      try {
+        const response = await fetch(`${API_URL}/api/test`)
+
+        if (!response.ok) {
+          throw new Error('HTTP Error: Status ', response.status)
+        }
+
+        let data = await response.json()
+        console.log(data)
+        setData(data)
+        setError(null)
+
+      } catch (err) {
+        setError(err.message)
+        console.error('Error fetching data: ', err.message)
+        setData(null)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    dataFetchTest()
+
+  }, [API_URL])
+
+  let dataContent
+
+  if (loading) {
+    dataContent = <p>Loading data...</p>
+  } else if (error) {
+    dataContent = <p style={{ color: 'red' }}>Error fetching data: {error}</p>
+  } else if (data && data.message) {
+    dataContent = <p>{data.message}</p>
+  } else {
+    dataContent = <p>No data recieved.</p>
+  }
 
   return (
     <>
@@ -17,6 +61,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>{dataContent}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
