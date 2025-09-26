98b99549870d6a3623250360d2f82d5776851c2e
Mizakson <mizakson.dev@gmail.com>
2025-09-26 16:58:45 -0400

    Create ErrorPage.jsx

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function ErrorPage() {
+    return (
+        <div className="error-page-container">
+            <h1>Error: this route doesn't exist...</h1>
+            <p>Go back <Link to='/'>home</Link></p>
+        </div>
+    )
+}
+
+export default ErrorPage
