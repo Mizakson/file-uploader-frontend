106297b47729c657b69f432253b2b5dcec9bed30
Mizakson <mizakson.dev@gmail.com>
2025-09-26 16:52:21 -0400

    Add basic routing for index, login and sign-up pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import '../src/css/index.css'
-import App from './App.jsx'
+
+import Homepage from './pages/Homepage.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Login from './pages/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+    errorElement: <div>There has been an error :(</div>
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+    errorElement: <div>There has been an error :(</div>
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <div>There has been an error :(</div>
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
