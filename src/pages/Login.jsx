dbc44e3e7fbac848855de71a68f1b5f367e175db
Mizakson <mizakson.dev@gmail.com>
2025-09-27 15:19:31 -0400

    Revert "Remove redundant link tag"
    This reverts commit dc93bf1d461ed452d13cca03b6ec58a104a997a7.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom"
 
-// TODO: create and test login page
 function Login() {
     return (
         <div className="login-container">
             <h1>Hi from login page</h1>
-            {/* logout button here */}
+            <p>Go back <Link to='/'>home</Link></p>
         </div>
     )
 }
