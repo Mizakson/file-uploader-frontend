e3c33bf2c02f10b5fa72637aa62445e7c70f9949
Mizakson <mizakson.dev@gmail.com>
2025-09-27 14:30:24 -0400

    Add AuthProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import '../src/css/index.css'
 
+import { AuthProvider } from './contexts/AuthContext.jsx'
+
 import Homepage from './pages/Homepage.jsx'
 import SignUp from './pages/SignUp.jsx'
 import Login from './pages/Login.jsx'
+// import Profile from './pages/Profile.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
+// import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 const router = createBrowserRouter([
   {
@@ -24,10 +28,24 @@ const router = createBrowserRouter([
     element: <Login />,
     errorElement: <ErrorPage />
   },
+  /* 
+  {
+    element: <ProtectedRoute />, 
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/profile",
+        element: <Profile />,
+      }
+    ]
+  }
+  */
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
